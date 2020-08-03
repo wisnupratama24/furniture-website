b6f4c5b873cbbbce288016675ae47fdedd9260e7
wisnupratama24 <wisnuputrapratama24@gmail.com>
2020-08-03 21:37:01 +0700

    configuration sass, download package react-router-dom and sass, add colorVariables file in sass

---
+++ src/parts/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="spacing-sm">
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light"></nav>
+      </div>
+    </header>
+  );
+}
