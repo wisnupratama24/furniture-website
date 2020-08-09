ec68739359ab9808162d72c1fb612725cada1105
wisnupratama24 <wisnuputrapratama24@gmail.com>
2020-08-09 22:21:22 +0700

    make cards parts

--- src/parts/Card.js
+++ src/parts/Card.js
@@ -1,29 +1,31 @@
 import React from "react";
 import img_card from "assets/images/img-card.jpg";
+import Button from "elements/Button";
 export default function Card() {
   return (
-    <section className='container mt-5 mx-5'>
-      <div className='card'>
+    <section
+      className='container mt-5'
+      style={{ display: "flex", justifyContent: "center" }}>
+      <div className='card card-design'>
         <div className='row'>
-          <div className='col-md-4'>
-            <img src={img_card} className='w-100' alt='img' />
+          <div className='col-md-4 col-12'>
+            <img src={img_card} className='h-100 image-card' alt='img' />
           </div>
-          <div className='col-md-8'>
-            <div className='card-block'>
-              <h4 className='card-title'>Lorem ipsum dolor sit amet</h4>
-              <p className='card-text'>
-                Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat.{" "}
-              </p>
-              <p className='card-text'>
-                Duis aute irure dolor in reprehenderit in voluptate velit esse
-                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                cupidatat non proident, sunt in culpa qui officia deserunt
-                mollit anim id est laborum.
+          <div className='col-md-8 col-12 card-awards'>
+            <div className='card-block '>
+              <h3 className='card-title' style={{ width: 383 }}>
+                Design Awards For Badroom Ideas
+              </h3>
+              <p className='card-text' style={{ width: 383 }}>
+                “The problem with my house is that I have one open space to be
+                divided into living room, work space, kitchen, and dining area.
+                Hopefully, IKEA has the right solution. Thank you.” – Eva
+                Anastasia
               </p>
             </div>
+            <Button className='btn  font-weight-bold mt-3' hasShadow isPrimary>
+              Checkout
+            </Button>
           </div>
         </div>
       </div>
